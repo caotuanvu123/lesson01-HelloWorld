674bb5f9569a670efa0f23d5529f295356b6eb24
caotuanvu <caotuanvu198@gmail.com>
2018-10-05 12:25:33 +0700

    java_basic_operators

--- src/vu/dev/MainApp.java
+++ src/vu/dev/MainApp.java
@@ -1,7 +1,35 @@
 package vu.dev;
 
 public class MainApp {
-	  public static void main(String []args) {
-	      System.out.println("Hello World"); // prints Hello World
+	  public static void main(String []args)
+	  {
+	       int a = 9;
+	       int b = 5;
+	       int sum = a + b;
+	       System.out.println("Tong a + b = " + sum);
+	       
+	       int hieu = a -b;
+	       System.out.println("Hieu a -b = " + hieu);
+	       
+	       int thuong = a /b;
+	       System.out.println("thuong a / b = " + thuong);
+	       
+	       int chiaLayDu = a % b;
+	       System.out.println("chia lay du a % b = " + chiaLayDu);
+	       
+	       int Tich = a * b;
+	       System.out.println("thuong a * b = " + Tich);
+	       
+	       a +=  b;
+	       System.out.println("a +=b = " + a);
+	       
+	       a -= b;
+	       System.out.println("a -=b = " + a);
+	       
+	       a /= b;
+	       System.out.println("a /=b = " + a);
+	       
+	       a %= b;
+	       System.out.println("a %=b = " + a);
 	   }
 }
